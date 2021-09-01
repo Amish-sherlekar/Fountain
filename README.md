618cebcccc1fa39aae0bfb453ed60bf81e7e2054
Amish <82744820+Amish-sherlekar@users.noreply.github.com>
2021-09-01 19:23:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Fountain
+# TopplingBoxes
+Toppling Boxes
---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var bg;
+var boxes = [];
+var secondBoxes = [];
+var thirdBoxes = [];
+var fourthBoxes = [];
+var ground1, ground2, ground3;
+var goal1, goal2, goal3;
+function preload() {
+    bg = loadImage("download.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(500, 600);
+    colorMode(HSB)
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+
+    // box1 = new Box(230, 200, 20, 20);
+
+}
+
+function mouseClicked() {
+
+}
+
+function draw() {
+    background(0);
+    boxes.push(new Box(230, 200, random(6, 11)));
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].display();
+    }
+    secondBoxes.push(new Box(230, 200, random(6, 11)));
+    for (let b = 0; b < secondBoxes.length; b++) {
+        secondBoxes[b].display();
+    }
+    thirdBoxes.push(new Box(230, 200, random(6, 11)));
+    for (let a = 0; a < thirdBoxes.length; a++) {
+        thirdBoxes[a].display();
+    }
+    fourthBoxes.push(new Box(230, 200, random(6, 11)));
+    for (let c = 0; c < fourthBoxes.length; c++) {
+        fourthBoxes[c].display();
+    }
+    // fourthBoxes
+}
+
